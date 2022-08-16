3a74af8ec926bdf8e84879771f4e4c58f6b60452
FatihPc <fkilic1805@gmail.com>
2022-08-16 14:30:48 +0300

    Initial commit

---
+++ app/src/main/java/com/software3200/erzsprfkapp/ui/ProfileFragment.java
@@ -0,0 +1,37 @@
+package com.software3200.erzsprfkapp.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import com.software3200.erzsprfkapp.databinding.FragmentProfileBinding;
+import com.software3200.erzsprfkapp.databinding.FragmentSpportedBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/software3200/erzsprfkapp/ui/StoreFragment.java
@@ -0,0 +1,35 @@
+package com.software3200.erzsprfkapp.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.software3200.erzsprfkapp.databinding.FragmentNotificationsBinding;
+
+public class StoreFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
