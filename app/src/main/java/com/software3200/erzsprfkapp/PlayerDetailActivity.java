114d482643ffb4a93b793ed38e109bf4bbd956b0
FatihKilic1905 <fkilic1805@gmail.com>
2022-09-26 09:06:48 +0300

    260922 - 0906 - Laptop

---
+++ app/src/main/java/com/software3200/erzsprfkapp/PlayerDetailActivity.java
@@ -0,0 +1,13 @@
+package com.software3200.erzsprfkapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_detail);
+    }
+}
